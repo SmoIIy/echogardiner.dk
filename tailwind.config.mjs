02fb8deccfcbcdc1fe05152fd27e680a85496037
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-08-28 15:31:14 +0200

    Initial setup

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		colors: {
+			'main-white': '#2D2E28',
+			'main-dark-grey': '#2D2E28',
+			'light-brown': '#AC651A',
+			'dark-brown': '#663B1E',
+			'dark-blue': '#212830'
+		},
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+		// ...
+	],
+}
