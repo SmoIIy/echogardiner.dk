7e4255716a9a06c9d1a46f065137cf076518717a
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-09-02 12:53:01 +0200

    Contactblock opdatering

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,14 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-		colors: {
+		extend: {		
+			colors: {
 			'main-white': '#F2F2F2',
 			'main-dark-grey': '#2D2E28',
 			'light-brown': '#AC651A',
 			'dark-brown': '#663B1E',
 			'dark-blue': '#212830'
-		},
+		},},
+
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
